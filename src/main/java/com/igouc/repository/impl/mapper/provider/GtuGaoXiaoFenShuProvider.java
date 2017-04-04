e669ac2b901cd236042f986604f03f93f4a79943
gc <gc@gcs-MacBook-Pro.local>
2017-04-04 20:50:09 +0800

    change sql

--- src/main/java/com/igouc/repository/impl/mapper/provider/GtuGaoXiaoFenShuProvider.java
+++ src/main/java/com/igouc/repository/impl/mapper/provider/GtuGaoXiaoFenShuProvider.java
@@ -7,18 +7,10 @@ public class GtuGaoXiaoFenShuProvider {
     private static final String TABLE = "gtu_gaoxiao_fenshu";
 
     public String getGtuGaoXiaoFenShuById(Integer arg0) {
-        SQL sql = new SQL();
-        sql.SELECT(ALL_FIELD).FROM(TABLE);
-        sql.WHERE("id = #{arg0}");
-        return sql.toString();
+        return String.format(" select %s from %s where id = %s ", ALL_FIELD, TABLE, arg0);
     }
 
     public String getGtuGaoXiaoFenShuByLastIdAndLimit(Integer arg0, Integer arg1) {
-        SQL sql = new SQL();
-        sql.SELECT(ALL_FIELD).FROM(TABLE);
-        sql.WHERE(" id > #{arg0}");
-        sql.ORDER_BY(String.format(" id limit %d", arg1));
-
-        return sql.toString();
+        return String.format(" select %s from %s where id > %s order by id asc limit %s ", ALL_FIELD, TABLE, arg0, arg1);
     }
 }
